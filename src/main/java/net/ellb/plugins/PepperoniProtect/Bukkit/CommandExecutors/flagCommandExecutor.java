17ef4c12cb893aee422e6923f11440a31babfd3a
Elliot Beskow <minecraftellb@gmail.com>
2012-02-15 20:01:13 +0100

    Created new CommandExecutors

---
+++ src/main/java/net/ellb/plugins/PepperoniProtect/Bukkit/CommandExecutors/flagCommandExecutor.java
@@ -0,0 +1,20 @@
+/*
+ * PepperoniProtect
+ * Copyright (C) 2012 EllB <http://www.ellb.net/>
+ * 
+ * This program is a part of The SpicyPack and is
+ * therefore licensed under the SpicyCode custom
+ * license <http://www.plugins.ellb.net/license/>.
+ *
+ */
+package net.ellb.plugins.PepperoniProtect.Bukkit.CommandExecutors;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class flagCommandExecutor implements CommandExecutor {
+    public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
