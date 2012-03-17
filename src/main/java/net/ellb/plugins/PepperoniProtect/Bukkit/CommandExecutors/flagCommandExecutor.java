553ec69958dd4a6e8c0eb519816caa3379b36626
Elliot Beskow <minecraftellb@gmail.com>
2012-03-17 15:49:31 +0100

    Happy with the flag command changes :D (still untested).

--- src/main/java/net/ellb/plugins/PepperoniProtect/Bukkit/CommandExecutors/flagCommandExecutor.java
+++ src/main/java/net/ellb/plugins/PepperoniProtect/Bukkit/CommandExecutors/flagCommandExecutor.java
@@ -20,26 +20,27 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class flagCommandExecutor implements CommandExecutor {
-
+    
     PepperoniProtect plugin;
     private GlobalAreaManager areaManager;
     private FlagManager flagManager;
     private static String instructions = "To set a flag, type " + ChatColor.DARK_GREEN + "/flag [FLAGNAME] [VALUE]" + ChatColor.WHITE + ". Where " + ChatColor.DARK_GREEN + "[FLAGNAME]" + ChatColor.WHITE + " is the name of the flag you want to set, and " + ChatColor.DARK_GREEN + "[VALUE]" + ChatColor.WHITE + "is the value you want to set it to.";
-
+    
     public flagCommandExecutor(PepperoniProtect p) {
         this.plugin = p;
         this.areaManager = p.getAreaManager();
         this.flagManager = p.getFlagManager();
     }
-
+    
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player;
         if (sender instanceof Player) {
             player = (Player) sender;
-            if (flagManager.getFlagByDisplayName(args[0]) == null) {
+            PepperoniAreaFlagInfo flag = flagManager.getFlagByDisplayName(args[0]);
+            if (flag == null) {
                 player.sendMessage(ChatColor.RED + "That flag does not exist, please check your spelling.");
             } else {
-                if (player.hasPermission("pepperoni.flag.set." + flagManager.getFlagByDisplayName(args[0]))) {
+                if (player.hasPermission("pepperoni.flag.set." + flag.getPath())) {
                     if (areaManager.getArea(player.getLocation()) == null) {
                         player.sendMessage(ChatColor.RED + "You need to be inside an area to be able to change the flag.");
                     } else {
@@ -52,15 +53,19 @@ public class flagCommandExecutor implements CommandExecutor {
                                 case 1:
                                     player.sendMessage(instructions);
                                 case 2:
+                                    areaManager.getArea(player.getLocation()).setFlag(flag.getPath(), args);
+                                    player.sendMessage("Succesfully changed area flag " + flag.getDisplayName() + " to " + args[1]);
+                                default:
+                                    player.sendMessage(ChatColor.RED + "Too many arguments:");
+                                    player.sendMessage(instructions);
                             }
                         }
                     }
-
                 } else {
                     player.sendMessage(ChatColor.RED + "Sorry, but you aren't allowed to set that flag.");
                 }
             }
-
+            
         } else {
             sender.sendMessage(ChatColor.RED + "Sorry, you are not a real player, just a console. Please go in-game.");
         }
@@ -76,7 +81,7 @@ public class flagCommandExecutor implements CommandExecutor {
         }
         return true;
     }
-
+    
     private void sendFlagInfo(Player p) {
         p.sendMessage("-- Aviable Flags --");
         for (PepperoniAreaFlagInfo flag : plugin.getFlagManager().getFlags()) {
